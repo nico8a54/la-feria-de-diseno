e5f180116419afa76bec64b984f1b73c13e4cbb5
Carlos Alberto Gaviria Gomez <gaviria.carlos@correounivalle.edu.co>
2022-01-19 06:41:22 -0500

    v0.0.9

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,11 +4,13 @@ const line1 = document.querySelector(".bm-line1");
 const line2 = document.querySelector(".bm-line2");
 const nav = document.getElementById("bm-container");
 const menu = document.getElementById("navigator");
+const header = document.getElementById("header");
 
 const openMenu = () => {
     return new Promise((resolve, reject) => {
         line1.style.animation = "playLine1 300ms forwards";
         line2.style.animation = "playLine2 300ms forwards";
+        header.style.mixBlendMode = "normal";
         menu.style.display = "flex";
         menu.style.transitionDuration = "0.6s";
         setTimeout(()=>{
@@ -22,6 +24,7 @@ const closeMenu = () => {
     return new Promise((resolve, reject) => {
         line1.style.animation = "backLine1 300ms forwards";
         line2.style.animation = "backLine2 300ms forwards";
+        header.style.mixBlendMode = "difference";
         menu.style.opacity = 0;
         menu.style.transitionDuration = "0.2s";
         setTimeout(()=>{
@@ -47,3 +50,29 @@ menu.onclick = (e) => {
         console.log(e);
     }
 }
+
+// Detección de slide y cambio de estado
+UIkit.util.on('#slider-hero', 'itemshow', (e) => {
+    switch (e.detail[0].index) {
+        case 0:
+            document.getElementById("slider-1").style.width = "100%";
+            document.getElementById("slider-2").style.width = "0%";
+            document.getElementById("slider-3").style.width = "0%";
+            break;
+
+        case 1:
+            document.getElementById("slider-1").style.width = "100%";
+            document.getElementById("slider-2").style.width = "100%";
+            document.getElementById("slider-3").style.width = "0%";
+            break;
+
+        case 2:
+            document.getElementById("slider-1").style.width = "100%";
+            document.getElementById("slider-2").style.width = "100%";
+            document.getElementById("slider-3").style.width = "100%";
+            break;
+    
+        default:
+            break;
+    }
+});
