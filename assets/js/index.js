2f91eee1f67edf6eeac34ea65d820d7358e99e09
Carlos Alberto Gaviria Gomez <gaviria.carlos@correounivalle.edu.co>
2022-01-19 22:25:43 -0500

    v0.0.10
    Corrección de Altura de Hero
    Ajuste en animación de indicador de slider

--- assets/js/index.js
+++ assets/js/index.js
@@ -52,24 +52,60 @@ menu.onclick = (e) => {
 }
 
 // Detección de slide y cambio de estado
+var stateSlider = 0;
 UIkit.util.on('#slider-hero', 'itemshow', (e) => {
     switch (e.detail[0].index) {
         case 0:
-            document.getElementById("slider-1").style.width = "100%";
-            document.getElementById("slider-2").style.width = "0%";
-            document.getElementById("slider-3").style.width = "0%";
+            if (stateSlider == 0) {
+                document.getElementById("slider-1").style.width = "100%";
+                document.getElementById("slider-2").style.width = "0%";
+                document.getElementById("slider-3").style.width = "0%";
+            } else if (stateSlider == 1) {
+                document.getElementById("slider-1").style.width = "100%";
+                document.getElementById("slider-2").style.transition = "width 0.3s ease-in";
+                document.getElementById("slider-2").style.width = "0%";
+                document.getElementById("slider-3").style.width = "0%";
+            } else if (stateSlider == 2) {
+                document.getElementById("slider-1").style.width = "100%";
+                document.getElementById("slider-2").style.transition = "none";
+                document.getElementById("slider-2").style.width = "0%";
+                document.getElementById("slider-3").style.transition = "none";
+                document.getElementById("slider-3").style.width = "0%";
+            }
+            stateSlider = 0;
             break;
 
         case 1:
-            document.getElementById("slider-1").style.width = "100%";
-            document.getElementById("slider-2").style.width = "100%";
-            document.getElementById("slider-3").style.width = "0%";
+            if (stateSlider == 0) {
+                document.getElementById("slider-1").style.width = "100%";
+                document.getElementById("slider-2").style.transition = "width 0.3s ease-in";
+                document.getElementById("slider-2").style.width = "100%";
+                document.getElementById("slider-3").style.width = "0%";
+            } else if (stateSlider == 2) {
+                document.getElementById("slider-1").style.width = "100%";
+                document.getElementById("slider-2").style.transition = "width 0.3s ease-in";
+                document.getElementById("slider-2").style.width = "100%";
+                document.getElementById("slider-3").style.transition = "width 0.3s ease-in";
+                document.getElementById("slider-3").style.width = "0%";
+            }
+            stateSlider = 1;
             break;
 
         case 2:
-            document.getElementById("slider-1").style.width = "100%";
-            document.getElementById("slider-2").style.width = "100%";
-            document.getElementById("slider-3").style.width = "100%";
+            if (stateSlider == 0) {
+                document.getElementById("slider-1").style.width = "100%";
+                document.getElementById("slider-2").style.transition = "none";
+                document.getElementById("slider-2").style.width = "100%";
+                document.getElementById("slider-3").style.transition = "none";
+                document.getElementById("slider-3").style.width = "100%";
+            } else if (stateSlider == 1) {
+                document.getElementById("slider-1").style.width = "100%";
+                document.getElementById("slider-2").style.transition = "width 0.3s ease-in";
+                document.getElementById("slider-2").style.width = "100%";
+                document.getElementById("slider-3").style.transition = "width 0.3s ease-in";
+                document.getElementById("slider-3").style.width = "100%";
+            }
+            stateSlider = 2;
             break;
     
         default:
