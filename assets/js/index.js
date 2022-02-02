dcf4c3633861586e7bf8fb9d008a2be73ba4def4
Carlos Alberto Gaviria Gomez <gaviria.carlos@correounivalle.edu.co>
2022-02-02 18:21:08 -0500

    v0.1.10

--- assets/js/index.js
+++ assets/js/index.js
@@ -39,3 +39,31 @@ menuItems.item(2).addEventListener('click', () => {
 menuItems.item(3).addEventListener('click', () => {
   menuContent.classList.toggle('menu-active');
 });
+
+// Boton WhatsApp INICIO
+
+let Inicio_contenido = document.querySelector("section.nosotros div.container-1");
+let Inicio_contenido_Style = window.getComputedStyle(Inicio_contenido);
+
+// Boton WhatsApp FIN
+
+document.body.addEventListener('scroll', () => {
+  let coords = Inicio_contenido.getBoundingClientRect();
+  // console.log(coords);
+  // let ContenidoRight = Inicio_contenido_Style.getPropertyValue('margin-right');
+  // let coordscontenido = Inicio_contenido.getBoundingClientRect();
+
+  if (coords.top < 0) {
+    btnMenu.style.position = "fixed";
+    menuItems.item(1).style.position = "fixed";
+    menuItems.item(2).style.position = "fixed";
+    menuItems.item(3).style.position = "fixed";
+  }
+  else {
+    btnMenu.style.position = "absolute";
+    menuItems.item(1).style.position = "absolute";
+    menuItems.item(2).style.position = "absolute";
+    menuItems.item(3).style.position = "absolute";
+  }
+});
+// Menu para moviles, scroll FIN
