039a5cceebb49da386fdf3c93862305c5366f614
Carlos Alberto Gaviria Gomez <gaviria.carlos@correounivalle.edu.co>
2022-02-03 17:52:08 -0500

    v0.2.1

--- assets/js/index.js
+++ assets/js/index.js
@@ -77,3 +77,14 @@ document.body.addEventListener('scroll', () => {
   }
 });
 // Menu para moviles, scroll FIN
+
+const cards = document.getElementById("cards");
+
+cards.addEventListener('click', (e) => {
+  if (e.target && e.target.tagName == 'SPAN' ) {
+    e.target.parentElement.parentElement.classList.toggle('expand');
+  } else {
+    
+  }
+  console.log(e.target.parentElement);
+});
