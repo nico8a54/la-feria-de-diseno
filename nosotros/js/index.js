ed0ec1389f1f2b5281967b8528e970e9af0ad32b
Carlos Alberto Gaviria Gomez <gaviria.carlos@correounivalle.edu.co>
2022-02-07 09:40:25 -0500

    v0.2.4

--- nosotros/js/index.js
+++ nosotros/js/index.js
@@ -13,3 +13,32 @@
 //             }
 //         }
 // });
+
+const timeline0 = document.getElementById("timeline-0");
+const timeline1 = document.getElementById("timeline-1");
+const timeline2 = document.getElementById("timeline-2");
+const timeline3 = document.getElementById("timeline-3");
+const timeline4 = document.getElementById("timeline-4");
+const timeline5 = document.getElementById("timeline-5");
+
+window.addEventListener('resize', () => {
+    let whidthPage = document.documentElement.clientWidth;
+    console.log(document.documentElement.clientWidth);
+    if (whidthPage >= 600) {
+        timeline0.src = "./assets/timeline-pc-0.svg";
+        timeline1.src = "./assets/timeline-pc-1.svg";
+        timeline2.src = "./assets/timeline-pc-2.svg";
+        timeline3.src = "./assets/timeline-pc-3.svg";
+        timeline4.src = "./assets/timeline-pc-4.svg";
+        timeline5.src = "./assets/timeline-pc-5.svg";
+        console.log("holi");
+    } else {
+        timeline0.src = "./assets/timeline-mobile-0.svg";
+        timeline1.src = "./assets/timeline-mobile-1.svg";
+        timeline2.src = "./assets/timeline-mobile-2.svg";
+        timeline3.src = "./assets/timeline-mobile-3.svg";
+        timeline4.src = "./assets/timeline-mobile-4.svg";
+        timeline5.src = "./assets/timeline-mobile-5.svg";
+    }
+    
+})
