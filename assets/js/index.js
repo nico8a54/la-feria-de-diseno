87f04c621e5cebe40e05b997e1c929a5d6942271
Carlos Alberto Gaviria Gomez <gaviria.carlos@correounivalle.edu.co>
2022-02-07 14:23:46 -0500

    v0.2.11

--- assets/js/index.js
+++ assets/js/index.js
@@ -9,3 +9,34 @@ cards.addEventListener('click', (e) => {
   }
   // console.log(e.target.parentElement);
 });
+
+var iframe1 = document.getElementById('iframe-1');
+var player1 = new Vimeo.Player(iframe1);
+var iframe2 = document.getElementById('iframe-2');
+var player2 = new Vimeo.Player(iframe2);
+var iframe3 = document.getElementById('iframe-3');
+var player3 = new Vimeo.Player(iframe3);
+
+player1.on('play', () => {
+    console.log("Video 1 cargado!");
+    // spinnerLanding.style.opacity = 0;
+    // setTimeout(()=>{
+    //     spinnerLanding.style.display = "none";
+    // }, 1000);
+});
+
+player2.on('play', () => {
+  console.log("Video 2 cargado!");
+  // spinnerLanding.style.opacity = 0;
+  // setTimeout(()=>{
+  //     spinnerLanding.style.display = "none";
+  // }, 1000);
+});
+
+player3.on('play', () => {
+  console.log("Video 3 cargado!");
+  // spinnerLanding.style.opacity = 0;
+  // setTimeout(()=>{
+  //     spinnerLanding.style.display = "none";
+  // }, 1000);
+});
