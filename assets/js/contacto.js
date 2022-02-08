4236c7aeb00e88a1ba33bca2cc29e7cece6d35e4
Carlos Alberto Gaviria Gomez <gaviria.carlos@correounivalle.edu.co>
2022-02-08 04:39:37 -0500

    0.2.18

--- assets/js/contacto.js
+++ assets/js/contacto.js
@@ -14,20 +14,16 @@ const politica = new MDCCheckbox(document.querySelector('.politica'));
 
 // Acordion Contacto
 var acc = document.getElementsByClassName("btn-accordion");
-var i;
 
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
-    } else {
-      panel.style.display = "block";
-    }
-  });
-}
-
+acc[0].addEventListener("click", function() {
+  this.classList.toggle("active");
+  var panel = this.nextElementSibling;
+  if (panel.style.display === "block") {
+    panel.style.display = "none";
+  } else {
+    panel.style.display = "block";
+  }
+});
 
 // mdc.ripple.MDCRipple.attachTo(document.querySelector('.enviar'));
 // const enviar = document.querySelector('.enviar');
