82789300e4c2c2a481da91ddfc2a4e3dabd44daf
Carlos Alberto Gaviria Gomez <gaviria.carlos@correounivalle.edu.co>
2022-02-08 02:41:53 -0500

    v0.2.15

--- assets/js/contacto.js
+++ assets/js/contacto.js
@@ -1,4 +1,18 @@
-// Seccion Contacto
+
+const MDCTextField = mdc.textField.MDCTextField;
+const MDCSelect = mdc.select.MDCSelect;
+const MDCCheckbox = mdc.checkbox.MDCCheckbox;
+
+const nombre = new MDCTextField(document.querySelector('.nombre'));
+const empresa = new MDCTextField(document.querySelector('.empresa'));
+const ciudad = new MDCTextField(document.querySelector('.ciudad'));
+const correo = new MDCTextField(document.querySelector('.correo'));
+const celular = new MDCTextField(document.querySelector('.celular'));
+const instagram = new MDCTextField(document.querySelector('.instagram'));
+const categoria = new MDCSelect(document.querySelector('.categoria'));
+const politica = new MDCCheckbox(document.querySelector('.politica'));
+
+// Acordion Contacto
 var acc = document.getElementsByClassName("btn-accordion");
 var i;
 
@@ -14,11 +28,59 @@ for (i = 0; i < acc.length; i++) {
   });
 }
 
-mdc.textField.MDCTextField.attachTo(document.querySelector('.nombre'));
-mdc.textField.MDCTextField.attachTo(document.querySelector('.empresa'));
-mdc.textField.MDCTextField.attachTo(document.querySelector('.ciudad'));
-mdc.textField.MDCTextField.attachTo(document.querySelector('.correo'));
-mdc.textField.MDCTextField.attachTo(document.querySelector('.celular'));
-mdc.textField.MDCTextField.attachTo(document.querySelector('.instagram'));
-mdc.select.MDCSelect.attachTo(document.querySelector('.categoria'));
-mdc.checkBox.MDCCheckBox.attachTo(document.querySelector('.politica'));
+
+// mdc.ripple.MDCRipple.attachTo(document.querySelector('.enviar'));
+// const enviar = document.querySelector('.enviar');
+
+const sendForm = async () => {
+  // console.log(nombre.value, empresa.value, ciudad.value, correo.value, celular.value, instagram.value, categoria.value, politica.checked);
+  if ((politica.checked) && (nombre.value) && (correo.value) && (categoria.value != "")) {
+
+
+    const formObject = {
+      IsExcludedFromCampaigns: "true",
+      Name: nombre.value,
+      Email: correo.value
+    }
+
+      // attributes:{
+      //     nombre: formNombre.value,
+      //     apellidos: formApellido.value,
+      //     tipoDocumento: formTipoDocumento.value,
+      //     numDocumento: formNumDocumento.value,
+      //     empresa: formEmpresa.value,
+      //     canal: formCanal.value
+      // }
+
+    // Autenticacion de API
+    let myHeaders = new Headers();
+    myHeaders.append("Authorization", "Basic YTUwYmNjYjIyYmZjMTI1MDk3M2FmNmY3N2Y4NjEzODU6NjQzYTk2ZWFhZDZlODk5ZjI1MmI5NWRjNDNlYjZiM2I=");
+    myHeaders.append("Content-Type", "application/json");
+
+    let raw = JSON.stringify(formObject);
+
+    let requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow',
+    };
+
+    let sendResponse = await fetch("https://api.mailjet.com/v3/REST/contact", requestOptions);
+    let sendResponseJSON = await sendResponse.json();
+    
+    if (sendResponseJSON.Count >= 1) {
+      alert("Registrado correctamente!");
+    }
+    
+    else {
+        alert("Error al enviar formulario ", JSON.stringify(sendResponseJSON));
+    }
+
+  }
+
+  else {
+    alert("Rellena los espacios requeridos.");
+  }
+  
+}
