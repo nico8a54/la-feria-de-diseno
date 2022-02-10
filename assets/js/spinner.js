163416c28937f9c262830efa88131e0ffe9c034f
Carlos Alberto Gaviria Gomez <gaviria.carlos@correounivalle.edu.co>
2022-02-09 19:03:30 -0500

    v0.3.1

---
+++ assets/js/spinner.js
@@ -0,0 +1,17 @@
+const spinner = document.getElementById("spinner");
+
+function delaySpinner () {
+  spinner.style.display = "none";
+}
+
+function showSpinner () {
+  spinner.style.opacity = "0";
+  setTimeout (delaySpinner, 300);
+}
+
+// window.addEventListener("load",showSpinner);
+
+var iframe1 = document.getElementById('iframe-1');
+var player1 = new Vimeo.Player(iframe1);
+
+player1.on('play', showSpinner);
