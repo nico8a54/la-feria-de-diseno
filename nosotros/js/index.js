59187b0ab4bdd0740658c9ce3066f5f0779076c9
Carlos Alberto Gaviria Gomez <gaviria.carlos@correounivalle.edu.co>
2022-02-10 21:09:27 -0500

    v0.4.0.1c

--- nosotros/js/index.js
+++ nosotros/js/index.js
@@ -14,34 +14,64 @@
 //         }
 // });
 
-const timeline0 = document.getElementById("timeline-0");
-const timeline1 = document.getElementById("timeline-1");
-const timeline2 = document.getElementById("timeline-2");
-const timeline3 = document.getElementById("timeline-3");
-const timeline4 = document.getElementById("timeline-4");
-const timeline5 = document.getElementById("timeline-5");
+// const timeline0 = document.getElementById("timeline-0");
+// const timeline1 = document.getElementById("timeline-1");
+// const timeline2 = document.getElementById("timeline-2");
+// const timeline3 = document.getElementById("timeline-3");
+// const timeline4 = document.getElementById("timeline-4");
+// const timeline5 = document.getElementById("timeline-5");
 
 
-// window.addEventListener('load')
+// // window.addEventListener('load')
 
-window.addEventListener('resize', () => {
-    let whidthPage = document.documentElement.clientWidth;
-    console.log(document.documentElement.clientWidth);
-    if (whidthPage >= 600) {
-        timeline0.src = "./assets/timeline-pc-0.svg";
-        timeline1.src = "./assets/timeline-pc-1.svg";
-        timeline2.src = "./assets/timeline-pc-2.svg";
-        timeline3.src = "./assets/timeline-pc-3.svg";
-        timeline4.src = "./assets/timeline-pc-4.svg";
-        timeline5.src = "./assets/timeline-pc-5.svg";
-        console.log("holi");
-    } else {
-        timeline0.src = "./assets/timeline-mobile-0.svg";
-        timeline1.src = "./assets/timeline-mobile-1.svg";
-        timeline2.src = "./assets/timeline-mobile-2.svg";
-        timeline3.src = "./assets/timeline-mobile-3.svg";
-        timeline4.src = "./assets/timeline-mobile-4.svg";
-        timeline5.src = "./assets/timeline-mobile-5.svg";
-    }
+// window.addEventListener('resize', () => {
+//     let whidthPage = document.documentElement.clientWidth;
+//     console.log(document.documentElement.clientWidth);
+//     if (whidthPage >= 600) {
+//         timeline0.src = "./assets/timeline-pc-0.svg";
+//         timeline1.src = "./assets/timeline-pc-1.svg";
+//         timeline2.src = "./assets/timeline-pc-2.svg";
+//         timeline3.src = "./assets/timeline-pc-3.svg";
+//         timeline4.src = "./assets/timeline-pc-4.svg";
+//         timeline5.src = "./assets/timeline-pc-5.svg";
+//         console.log("holi");
+//     } else {
+//         timeline0.src = "./assets/timeline-mobile-0.svg";
+//         timeline1.src = "./assets/timeline-mobile-1.svg";
+//         timeline2.src = "./assets/timeline-mobile-2.svg";
+//         timeline3.src = "./assets/timeline-mobile-3.svg";
+//         timeline4.src = "./assets/timeline-mobile-4.svg";
+//         timeline5.src = "./assets/timeline-mobile-5.svg";
+//     }
     
+// })
+const timeline = document.getElementById("timeline")
+const navBar = document.getElementById("navbar-example")
+const section4 = document.getElementById("section4")
+const section5 = document.getElementById("section5")
+
+window.addEventListener('scroll', () => {
+    let coords = timeline.getBoundingClientRect();
+    let coordsSection4 = section4.getBoundingClientRect();
+    let coordsSection5 = section5.getBoundingClientRect();
+    // console.log(coordsSection5);
+    if ((coords.top < 0) && (coordsSection5.top > 0)) {
+        navBar.style.position = "fixed";
+        navBar.style.top = "0";
+    } 
+    else if ((coords.top < 0) && (coordsSection5.top <= 0)) {
+        navBar.style.position = "absolute";
+        navBar.style.top = "inherit";
+        navBar.style.bottom = "0";
+    }
+    else {
+        navBar.style.position = "absolute";
+        navBar.style.top = "0";
+    }
+
+    if (coordsSection4.top < 400) {
+        navBar.classList.add("dark");
+    } else {
+        navBar.classList.remove("dark")
+    }
 })
